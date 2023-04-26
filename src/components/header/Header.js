2c60da10cb019533a0d0362e32460b5d21500323
Sashka.Chi <cirtoska@gmail.com>
2023-04-26 20:09:17 +0200

    mobile menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/shared/logo.svg";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
     <header>
       <nav>
         <div>
           <img src={logo} alt="space tourism logo" className="logo" />
         </div>
-        <ul>
+        {/* <button
+          className="close-btn"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <GrClose className="close-icon" />
+        </button> */}
+        <button
+          className="hamburger-btn"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          {isNavExpanded ? (
+            <IoClose className="close-icon" />
+          ) : (
+            <GiHamburgerMenu className="hamburger-icon" />
+          )}
+        </button>
+        <ul className={isNavExpanded ? "nav-links expanded" : "nav-links"}>
           <li className="nav-link">
             <NavLink to="/">
               <span className="nav-num">00</span>Home
