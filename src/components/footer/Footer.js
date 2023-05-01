fe69930a43e9f97016a7a019cf43070340f78a3c
Sashka.Chi <cirtoska@gmail.com>
2023-05-01 08:45:06 +0200

    starting styling destination page

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./style.css";
+import { FaRobot } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <FaRobot className="icon" />
+      <p className="created">
+        Created by <span className="signature">SashkaChi.</span> &copy;{" "}
+        {new Date().getFullYear()}
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/destination/Destination.js
+++ src/pages/destination/Destination.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./style.css";
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import image from "../../assets/destination/image-moon.webp";
 
 const Destination = () => {
   return (
-    <div>
+    <div className="destination">
       <Header />
+      <section className="main-destination flex container">
+        <article className="destination-image">
+          <h5>
+            <span className="menu-num">01</span> PICK YOUR DESTINATION
+          </h5>
+          <img src={image} alt="" />
+        </article>
+        <article className="destination-info"></article>
+      </section>
       <Footer />
     </div>
   );
